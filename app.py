e4da39ef769187fe63d6a6a94cc8014a6739c0fe
Tyler <48775431+Thymester@users.noreply.github.com>
2025-03-27 22:27:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-# app.py
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import secrets  # Import the 'secrets' module for secure key generation
+import os  # To fetch the correct port in production
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(32)  # Generate a 256-bit secret key
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")  # Enable CORS for WebSocket connections
 
 @app.route('/')
 def index():
@@ -15,8 +15,7 @@ def index():
 def handle_new_message(data):
     message = data['message']
     uid = data['uid']
-
     emit('message_received', {'message': message, 'uid': uid}, broadcast=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))  # Use socketio.run for WebSocket support
